a6b454704974f63c6bba04e89567f1ee0be8b612
rclank <53981655+rclank@users.noreply.github.com>
2021-11-28 18:00:12 -0500

    adding stuff

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-// const name  = profileDataArgs[0];
-// const github  = profileDataArgs[1];
-const [name, github] = profileDataArgs;
+// const pageHTML = generatePage(name, github);
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-    if (err) throw err;
+// fs.writeFile('./index.html', pageHTML, err => {
+//     if (err) throw err;
 
-    console.log('Porfolio complete! Check out index.html to see the output!');
-});
+//     console.log('Porfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
