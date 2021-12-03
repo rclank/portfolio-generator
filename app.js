754c344c700c4008860d552429a98d909e3be66a
rclank <53981655+rclank@users.noreply.github.com>
2021-12-02 22:47:47 -0500

    more stuff

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 const inquirer = require('inquirer');
-const fs = require('fs');
+const { writeFile, copyFile } = require('./utils/generate-site.js');
 const generatePage = require('./src/page-template.js');
 
 
 
-const promptUser = () => {
+let promptUser = () => {
     return inquirer.prompt([
         {
             type: 'input',
@@ -171,19 +171,38 @@ const mockData = {
     ]
 };
 
-const pageHTML = generatePage(mockData);
-fs.writeFile('./index.html', pageHTML, err => {
-    if (err) throw err;
-});
+// const pageHTML = generatePage(mockData);
+// fs.writeFile('./dist/index.html', pageHTML, err => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+// console.log('Page created! Check out the index.html in this directory to see it!');
 
-// promptUser()
-//     .then(promptProject)
-//     .then(portfolioData => {
-//         const pageHTML = generatePage(portfolioData);
+// fs.copyFile('./src/style.css', './dist/style.css', err => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log('Style sheet copied successfully');
+// });
+// });
 
-//         // fs.writeFile('./index.html', pageHTML, err => {
-//         //     if (err) throw err;
-
-//         //     console.log('Porfolio complete! Check out index.html to see the output!');
-//         // });
-//     });
+promptUser()
+    .then(promptProject)
+    .then(portfolioData => {
+        return generatePage(portfolioData);
+    })
+    .then(pageHTML => {
+        return writeFile(pageHTML);
+    })
+    .then(writeFileResponse => {
+        console.log(writeFileResponse);
+        return copyFile();
+    })
+    .then(copyFileResponse => {
+        console.log(copyFileResponse);
+    })
+    .catch(err => {
+        console.log(err);
+    });
